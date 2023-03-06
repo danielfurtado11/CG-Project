509a51360445a35f82779d8ed440aad5da5f4841
Ricardo Araújo <ricardoaraujo@Ricardos-Air.Home>
2023-03-06 22:42:00 +0000

    fe

--- engine.cpp
+++ engine.cpp
@@ -36,18 +36,27 @@ int main() {
     double near = projection->DoubleAttribute("near");
     double far = projection->DoubleAttribute("far");    
 
-    // get the models element and iterate over all its model child elements
-    tinyxml2::XMLElement* models = root->FirstChildElement("group")->FirstChildElement("models");
+    // declare a vector to store the model file names
+    std::vector<std::string> modelsList;
+
+    // iterate over all model child elements
     for (tinyxml2::XMLElement* model = models->FirstChildElement("model");
-         model != nullptr;
-         model = model->NextSiblingElement("model")) {  
+        model != nullptr;
+        model = model->NextSiblingElement("model")) {  
 
-            // extract the file attribute value of the current model element
-            const char* file = model->Attribute("file"); 
+        // extract the file attribute value of the current model element
+        const char* file = model->Attribute("file");
 
-            // print the file attribute value
-            std::cout << "Model file: " << file << std::endl;
+        // add the file attribute value to the vector
+        modelsList.push_back(file);
     }   
+
+// print the model file names stored in the vector
+std::cout << "Model files:\n";
+for (auto const& file : modelsList) {
+    std::cout << "- " << file << std::endl;
+}
+
 
     // print the extracted information
     std::cout << "Window width: " << width << ", height: " << height << std::endl;
