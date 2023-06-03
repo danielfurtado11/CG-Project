b6dc63add92b2f8ba0105fc3e2d78dd2dfc7e98e
Ricardo Araújo <ricardoaraujo@Ricardos-Air.home>
2023-06-03 23:33:15 +0100

    l

--- engine.cpp
+++ engine.cpp
@@ -8,11 +8,12 @@
 #include <math.h>
 #include <sstream>
 #include "fpsCamera.h"
+#include "lights.h"
 #include "model.h"
 #include "transformations.h"
 
 using namespace std;
-
+using namespace tinyxml2;
 #define MAX 100
 
 
@@ -22,6 +23,7 @@ GLdouble gamma_value = 40.0;
 
 GLenum modo = GL_FILL;
 std::vector<Group> groupList;
+vector<Light*> lights_vector = {};
 
 int timebase;
 float frames;
@@ -162,10 +164,37 @@ void drawGroup(Group g){
     std::vector<Model> modelsList = g.getModelsList();
 
     for(Model m : modelsList){
-        glBindBuffer(GL_ARRAY_BUFFER, m.getVBOInd());
+
+        // Set model material properties
+        glMaterialfv(GL_FRONT, GL_AMBIENT, m.getAmbient());
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, m.getDiffuse());
+        glMaterialfv(GL_FRONT, GL_SPECULAR, m.getSpecular());
+        glMaterialfv(GL_FRONT, GL_EMISSION, m.getEmissive());
+        glMaterialf(GL_FRONT, GL_SHININESS, m.getShininess());
+
+        // Bind points VBO
+        glBindBuffer(GL_ARRAY_BUFFER, m.getPVBOInd());
         glVertexPointer(3, GL_FLOAT, 0, 0);
+
+        // If defined, bind normals VBO
+        GLuint n_vbo_ind = m.getNVBOInd();
+        if (n_vbo_ind != 0) {
+            glBindBuffer(GL_ARRAY_BUFFER, n_vbo_ind);
+            glNormalPointer(GL_FLOAT, 0, 0);
+        }
+
+        // If defined, bind textures VBO
+        GLuint t_vbo_ind = m.getTVBOInd();
+        if (t_vbo_ind != 0) {
+            glBindBuffer(GL_ARRAY_BUFFER, t_vbo_ind);
+            glTexCoordPointer(2, GL_FLOAT, 0, 0);
+            glBindTexture(GL_TEXTURE_2D, m.getTextureID());
+        }
+
         glDrawArrays(GL_TRIANGLES, 0, m.getVerticeCount());
-        glScalef(1.0f,1.0f,1.0f);
+
+        glBindTexture(GL_TEXTURE_2D, 0);
+
     }
 
     // Drawing groups in this group
@@ -248,6 +277,9 @@ void renderScene(void) {
 	sprintf(s, "%f", fps);
 	glutSetWindowTitle(s);
 
+    for (Light* l : lights_vector) {
+		l->apply();
+	}
 
     for(Group g : groupList){
         drawGroup(g);
@@ -302,6 +334,108 @@ void processMouseMotion(int xx, int yy) {
 void processMouseButtons(int button, int state, int xx, int yy) {
 		fps_camera->processar_botoes_rato(button, state, xx, yy);
 		glutPostRedisplay();
+}
+
+
+
+Light* parseXMLLightElement (XMLElement* light_element, int light_ind) {
+	Light* new_light = nullptr;
+
+	// Get light type: POINT, DIRECTIONAL or SPOT
+	const XMLAttribute* type_attribute = light_element->FindAttribute("type");
+	const char* light_type;
+	type_attribute ? light_type = type_attribute->Value() : light_type = "";
+
+	// Get ambient attributes
+	GLfloat* ambient = parseAmbientAttributes(light_element, 0.0);
+
+	// Get diffuse attributes
+	GLfloat* diffuse = parseDiffuseAttributes(light_element, 1.0);
+
+	// Get specular attributes
+	GLfloat* specular = parseSpecularAttributes(light_element, 1.0);
+
+	if (strcmp(light_type, "POINT") == 0) {
+		float posX = parseFloatFromElementAttribute(light_element, "posX", 0.0);
+		float posY = parseFloatFromElementAttribute(light_element, "posY", 0.0);
+		float posZ = parseFloatFromElementAttribute(light_element, "posZ", 0.0);
+
+		new_light = new LightPoint(light_ind, new Ponto(posX, posY, posZ), ambient, diffuse, specular);
+	}
+	else if (strcmp(light_type, "DIRECTIONAL") == 0) {
+		float dirX = parseFloatFromElementAttribute(light_element, "dirX", 0.0);
+		float dirY = parseFloatFromElementAttribute(light_element, "dirY", 0.0);
+		float dirZ = parseFloatFromElementAttribute(light_element, "dirZ", 1.0);
+
+		new_light = new LightDirectional(light_ind, new Ponto(dirX, dirY, dirZ), ambient, diffuse, specular);
+	}
+	else if (strcmp(light_type, "SPOT") == 0) {
+		float posX = parseFloatFromElementAttribute(light_element, "posX", 0.0);
+		float posY = parseFloatFromElementAttribute(light_element, "posY", 0.0);
+		float posZ = parseFloatFromElementAttribute(light_element, "posZ", 0.0);
+
+		float dirX = parseFloatFromElementAttribute(light_element, "dirX", 0.0);
+		float dirY = parseFloatFromElementAttribute(light_element, "dirY", 0.0);
+		float dirZ = parseFloatFromElementAttribute(light_element, "dirZ", -1.0);
+
+		float cutoff = parseFloatFromElementAttribute(light_element, "cutoff", 180.0);
+
+		new_light = new LightSpot(light_ind, new Ponto(posX, posY, posZ), new Ponto(dirX, dirY, dirZ), cutoff, ambient, diffuse, specular);
+	}
+
+	return new_light;
+}
+
+// Function to parse ambient attributes in a light or model element
+GLfloat* parseAmbientAttributes(XMLElement* element, GLfloat default_value) {
+	const XMLAttribute* aux_attribute;
+	GLfloat* ambient = new GLfloat[4];
+	
+	ambient[0] = parseFloatFromElementAttribute(element, "ambiR", default_value);
+	ambient[1] = parseFloatFromElementAttribute(element, "ambiG", default_value);
+	ambient[2] = parseFloatFromElementAttribute(element, "ambiB", default_value);
+	ambient[3] = parseFloatFromElementAttribute(element, "ambiA", 1.0);
+
+	return ambient;
+}
+
+// Function to parse diffuse attributes in a light or model element
+GLfloat* parseDiffuseAttributes(XMLElement* element, GLfloat default_value) {
+	const XMLAttribute* aux_attribute;
+	GLfloat* diffuse = new GLfloat[4];
+	
+	diffuse[0] = parseFloatFromElementAttribute(element, "diffR", default_value);
+	diffuse[1] = parseFloatFromElementAttribute(element, "diffG", default_value);
+	diffuse[2] = parseFloatFromElementAttribute(element, "diffB", default_value);
+	diffuse[3] = parseFloatFromElementAttribute(element, "diffA", 1.0);
+
+	return diffuse;
+}
+
+// Function to parse specular attributes in a light or model element
+GLfloat* parseSpecularAttributes(XMLElement* element, GLfloat default_value) {
+	const XMLAttribute* aux_attribute;
+	GLfloat* specular = new GLfloat[4];
+	
+	specular[0] = parseFloatFromElementAttribute(element, "specR", default_value);
+	specular[1] = parseFloatFromElementAttribute(element, "specG", default_value);
+	specular[2] = parseFloatFromElementAttribute(element, "specB", default_value);
+	specular[3] = parseFloatFromElementAttribute(element, "specA", 1.0);
+
+	return specular;
+}
+
+// Function to parse emissive attributes in a model element
+GLfloat* parseEmissiveAttributes(XMLElement* element, GLfloat default_value) {
+	const XMLAttribute* aux_attribute;
+	GLfloat* emissive = new GLfloat[4];
+	
+	emissive[0] = parseFloatFromElementAttribute(element, "emisR", default_value);
+	emissive[1] = parseFloatFromElementAttribute(element, "emisG", default_value);
+	emissive[2] = parseFloatFromElementAttribute(element, "emisB", default_value);
+	emissive[3] = parseFloatFromElementAttribute(element, "emisA", 1.0);
+
+	return emissive;
 }
 
 
@@ -433,6 +567,29 @@ Group loadGroupXML(tinyxml2::XMLElement* child){
             std::cout << "Model: " << file << "\n";
             // add the file attribute value to the vector
             Model m = drawObject(file);
+
+            // Get diffuse attributes
+			GLfloat* diffuse = parseDiffuseAttributes(model_element, 0.8);
+			model.setDiffuse(diffuse);
+
+			// Get specular attributes
+			GLfloat* specular = parseSpecularAttributes(model_element, 0.0);
+			model.setSpecular(specular);
+
+			// Get emissive attributes
+			GLfloat* emissive = parseEmissiveAttributes(model_element, 0.0);
+			model.setEmissive(emissive);
+
+			// Get ambient attributes
+			GLfloat* ambient = parseAmbientAttributes(model_element, 0.2);
+			model.setAmbient(ambient);
+
+			// Get texture attribute
+			const XMLAttribute* texture_attribute = model_element->FindAttribute("texture");
+			string texture_file;
+			texture_attribute ? texture_file = texture_attribute->Value() : texture_file = "";
+			
+			model.loadTexture(BIN_IMAGE_DIR + texture_file);
             group.addModels(m);
         }
     }
@@ -483,6 +640,22 @@ void loadXML(char* ficheiro){
     double near = projection->DoubleAttribute("near");
     double far = projection->DoubleAttribute("far");    
 
+    // Trying to get lights element
+	tinyxml2::XMLElement* lights_element = root->FirstChildElement("lights");
+	if (lights_element) {
+		int light_ind = 0;
+		
+		tinyxml2::XMLElement* light_element = lights_element->FirstChildElement("light");
+		while (light_element) {
+			Light* l = parseXMLLightElement(light_element, light_ind);
+			if (l != nullptr)
+				lights_vector->push_back(l);
+
+			light_element = light_element->NextSiblingElement("light");
+			light_ind++;
+		}
+	}
+
     // Ler o group
     tinyxml2::XMLElement* grupos = root->FirstChildElement("group");
 	while (grupos) {
@@ -524,7 +697,15 @@ int main(int argc , char** argv) {
 
     //  OpenGL settings
 	glEnable(GL_DEPTH_TEST);
-
+    glEnable(GL_CULL_FACE);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_LIGHTING);
+	// Enable lights parsed in XML file
+	for (int i = 0; i < lights_vector.size(); i++)
+		glEnable(GL_LIGHT0 + (GLenum) i);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	fps_camera = new fpsCamera(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT), "fpscamera.cfg");
 	glutWarpPointer(glutGet(GLUT_WINDOW_WIDTH)/2, glutGet(GLUT_WINDOW_HEIGHT)/2);
